b75ae04fa2ed86a4cb64aa6aba8b43220763d503
apryamostanov <anton.pryamostanov@gmail.com>
2020-04-21 23:13:56 +0300

    Progress

---
+++ src/main/groovy/io/infinite/ascend/SecurityConfig.groovy
@@ -0,0 +1,35 @@
+package main.groovy.io.infinite.ascend
+
+import groovy.util.logging.Slf4j
+import io.infinite.blackbox.BlackBox
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter
+
+import javax.servlet.http.HttpServletResponse
+
+@EnableWebSecurity
+@Slf4j
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @BlackBox
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling().authenticationEntryPoint({
+            req, rsp, e ->
+                rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+        })
+                .and()
+                .authorizeRequests()
+                .antMatchers("/ascend/**").permitAll()
+                .anyRequest().authenticated()
+    }
+
+}
