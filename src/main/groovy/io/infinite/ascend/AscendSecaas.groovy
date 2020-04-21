2fa848372ab080f53737714ea26d59300054c8eb
apryamostanov <anton.pryamostanov@gmail.com>
2020-04-21 17:14:57 +0300

    Use Ascend 0.2.0

---
+++ src/main/groovy/io/infinite/ascend/AscendSecaas.groovy
@@ -0,0 +1,35 @@
+package main.groovy.io.infinite.ascend
+
+
+import groovy.util.logging.Slf4j
+import io.infinite.blackbox.BlackBox
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.hateoas.config.EnableHypermediaSupport
+
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@SpringBootApplication
+@Slf4j
+class AscendSecaas implements CommandLineRunner {
+
+    @Autowired
+    ConfigInitService configInitService
+
+    static void main(String[] args) {
+        SpringApplication.run(AscendSecaas.class, args)
+    }
+
+    @Override
+    void run(String... args) throws Exception {
+        runWithLogging()
+    }
+
+    @BlackBox
+    void runWithLogging() {
+        log.info("Starting Ascend...")
+        configInitService.initConfig()
+    }
+
+}
---
+++ src/main/groovy/io/infinite/ascend/ConfigInitService.groovy
@@ -0,0 +1,62 @@
+package main.groovy.io.infinite.ascend
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import io.infinite.ascend.granting.configuration.entities.*
+import io.infinite.ascend.granting.configuration.repositories.*
+import io.infinite.blackbox.BlackBox
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+@Slf4j
+@BlackBox
+@CompileStatic
+class ConfigInitService {
+
+    @Autowired
+    PrototypeGrantRepository grantRepository
+
+    @Autowired
+    PrototypeScopeRepository scopeRepository
+
+    @Autowired
+    PrototypeAuthenticationRepository authenticationTypeRepository
+
+    @Autowired
+    PrototypeIdentityRepository identityTypeRepository
+
+    @Autowired
+    PrototypeAuthorizationRepository authorizationTypeRepository
+
+    @BlackBox
+    void initConfig() {
+        Set<PrototypeGrant> grants = new HashSet<PrototypeGrant>()
+        log.info("Initializing config")
+        grants.add(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/%appName%\\/managedEmail"))
+        grants.add(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/%appName%\\/managedSms"))
+        grants.add(new PrototypeGrant(httpMethod: "GET", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/templates\\/search\\/findByAppName\\?appName=%appName%"))
+        grants.add(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/templates"))
+        grantRepository.saveAll(grants)
+        grantRepository.flush()
+        Set<PrototypeScope> scopes = new HashSet<PrototypeScope>()
+        scopes.add(new PrototypeScope(name: "Orbit", grants: grants))
+        scopeRepository.saveAll(scopes)
+        scopeRepository.flush()
+        Set<PrototypeAuthentication> authenticationTypes = new HashSet<PrototypeAuthentication>()
+        authenticationTypes.add(new PrototypeAuthentication(name: "JWT"))
+        authenticationTypeRepository.saveAll(authenticationTypes)
+        authenticationTypeRepository.flush()
+        Set<PrototypeIdentity> identityTypes = new HashSet<PrototypeIdentity>()
+        identityTypes.add(new PrototypeIdentity(name: "Trusted Application", authentications: authenticationTypes))
+        identityTypeRepository.saveAll(identityTypes)
+        identityTypeRepository.flush()
+        Set<PrototypeAuthorization> authorizationTypes = new HashSet<PrototypeAuthorization>()
+        authorizationTypes.add(new PrototypeAuthorization(name: "App2app", identities: identityTypes, scopes: scopes
+                , durationSeconds: 3000
+                , maxUsageCount: 100
+        ))
+        authorizationTypeRepository.saveAll(authorizationTypes)
+        authorizationTypeRepository.flush()
+    }
+}
